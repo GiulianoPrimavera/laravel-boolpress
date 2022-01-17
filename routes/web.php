62a041824bedb5ce6c773c654ba0d44128528340
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2022-01-17 15:53:45 +0100

    adds routes for the admin section

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use PhpParser\Node\Stmt\GroupUse;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +19,13 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+
+Route::middleware('auth')
+    ->namespace('Admim')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function(){
+        Route::get('/', 'HomeController@index')->name('home');
+});
 
 Route::get('/home', 'HomeController@index')->name('home');
