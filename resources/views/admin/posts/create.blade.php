7a43ae089d98dfa7413d08e1d31322e2d51edc28
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2022-01-18 17:12:31 +0100

    creates admin posts create page

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <h1>sezione di creazione posts</h1>
+        </div>
+
+    </div>
+@endsection
