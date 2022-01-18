057ee6de22eb952fa11e1825e03c4da39c3616b0
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2022-01-18 17:23:26 +0100

    adds main layout for the creation page

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -2,9 +2,21 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            <h1>sezione di creazione posts</h1>
-        </div>
+
+        <form action="">
+
+            <div class="form-group">
+                <label for="title">Inserisci il titolo del post</label>
+                <input type="text" id="title" class="form-control" name="title">
+            </div>
+
+            <div class="form-group">
+                <label for="content">Inserisci il contenuto del post</label>
+                <input type="text" id="content" class="form-control" name="content">
+            </div>
+
+            <button class="btn btn-success" type="submit">Crea</button>
+        </form>
 
     </div>
 @endsection
