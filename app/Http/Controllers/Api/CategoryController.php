bdabfbec86dadc40618d8927bf3cbba3616e6995
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2022-01-28 16:29:50 +0100

    craetes CategoryController that returns the selected category with the posts that have a relashionship with it

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Category;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function show($category){
+        /* dump($category);
+        exit; */
+        /***
+         * per fare chiarezza nella mia testa
+         * 1. L'argomento che viene passato alla funzione show, !è l'argomento variabile che viene letto nell'url
+         *      e che noi indichiamo nella Route:: con le parentesi {} -> Route::get("category/{category}", "Api\CategoryController@show")
+         * 2. il paragone che viene fatto nel where() è tra l'id di una categoria e il parametro che accetta la funzione  show()
+         * 3. il with() è da studiare come funziona, so solo che insieme alla tabella principale ci attacca tutte quelle che sono collegate tramite relazione a database 
+         *   */
+        $category = Category::where("id", $category)->with("post")->get();
+
+        return response()->json($category);
+    }
+}
